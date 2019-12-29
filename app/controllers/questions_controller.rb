0b40a1d57a5db4509ea46e0345ff08e14fab2334
syooooring <56821781+syooooring@users.noreply.github.com>
2019-12-29 12:42:17 +0900

    refactoring

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,10 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
   def index
     @questions = Question.all
   end
 
   def show
-    @question = Question.find(params[:id])
     @answer = Answer.new
   end
 
@@ -23,11 +23,9 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-    @question = Question.find(params[:id])
   end
 
   def update
-    @question = Question.find(params[:id])
     if @question.update(question_params)
       redirect_to root_path, notice: 'Success!'
     else
@@ -37,13 +35,17 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question = Question.find(params[:id])
     @question.destroy
     redirect_to root_path, notice: "Success!"
   end
 
 
   private
+
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
     def question_params
       params.require(:question).permit(:name, :title, :content)
     end
